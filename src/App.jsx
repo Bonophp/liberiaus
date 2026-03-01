bdf09dd26836af79d46da8b7d35c0f253136c44e
Bono <1224100662.jaab@gmail.com>
2026-03-01 17:15:15 -0600

    fix redirect google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { supabase } from './supabase'
 import Navbar from './components/Navbar'
 import Inicio from './pages/Inicio'
 import Libros from './pages/Libros'
@@ -6,6 +8,14 @@ import Listas from './pages/Listas'
 import LoginPage from './pages/LoginPage'
 
 function App() {
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        window.location.href = '/'
+      }
+    })
+  }, [])
+
   return (
     <BrowserRouter>
       <Navbar />
