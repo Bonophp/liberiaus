8af37f7a60b48e71afde61f8f9454bac421f8314
Bono <1224100662.jaab@gmail.com>
2026-03-01 17:01:23 -0600

    variables de entorno

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ksjqbxpbpivucybjlwid.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzanFieHBicGl2dWN5Ympsd2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzOTM2OTcsImV4cCI6MjA4Nzk2OTY5N30.o7tnMrb-WfNNE6YngmlLE4BQmLa7Jz3oKwGAwlNJg8c'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
